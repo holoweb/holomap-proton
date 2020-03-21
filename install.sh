40834ab2f88b2f5213f7bc0f56171894e4db21bb
larky <me@larky.io>
2020-03-21 18:37:52 +0000

    Updated install script to create sslcert directory

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ npm i
 echo; echo; echo;
 echo "Creating SSL keys for local development..."
 echo; echo; echo;
+mkdir sslcert
 ./genkeys.sh
 echo;echo;echo;
 echo "Building production file holomap.build.js"
